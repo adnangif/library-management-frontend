3fd29bcff7c184e0b5476432e3d4c3203b2eccd3
Fahim Adnan <59190871+adnangif@users.noreply.github.com>
2024-02-29 16:31:03 +0600

    added design for bookdetails page

--- src/components/book/BookDetailPage.tsx
+++ src/components/book/BookDetailPage.tsx
@@ -5,22 +5,23 @@ import SideNavBar from '../sidnav/SideNavBar';
 // import { useEffect, useState } from 'react';
 
 export default function BookDetailPage() {
-    const [title,setTitle] = useState('')
-    const [author,setAuthor] = useState('')
-    const [description,setDescription] = useState('')
-    const [ pyear, setPyear] = useState('')
-    const [edition,setEdition] = useState('')
+    const [title, setTitle] = useState('')
+    const [author, setAuthor] = useState('')
+    const [description, setDescription] = useState('')
+    const [pyear, setPyear] = useState('')
+    const [edition, setEdition] = useState('')
+    const [availablity, setAvailability] = useState("sdfas")
 
 
-    useEffect(()=>{
+    useEffect(() => {
         const book1 = {
-            title:"Introduction to algorithm",
-            author:"Dr. Somres Bosu",
+            title: "Introduction to algorithm",
+            author: "Dr. Somres Bosu",
             description: `Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-            Hic, quam impedit? Iste molestiae dolor blanditiis omnis ullam natus quas velit 
             provident dolorum id tempora praesentium iusto fuga, magnam cumque reprehenderit.`,
-            pyear:'2020',
-            edition:'6th',
+            pyear: '2020',
+            edition: '6th',
+            availablity: 'Available',
         }
 
         setTitle(book1.title)
@@ -28,10 +29,11 @@ export default function BookDetailPage() {
         setDescription(book1.description)
         setPyear(book1.pyear)
         setEdition(book1.edition)
+        setAvailability(book1.availablity)
 
-    },[])
+    }, [])
 
-    function handleOrder(){
+    function handleOrder() {
         console.log("order handling happens here")
     }
 
@@ -43,19 +45,36 @@ export default function BookDetailPage() {
         <div className='h-screen w-screen flex justify-center'>
             <div className='flex h-screen w-full lg:w-5/6 max-w-5xl  overflow-hidden bg-teal-950'>
                 <SideNavBar />
-                <div className='bg-neutral-800 text-neutral-100 border-2 border-teal-100 rounded-md max-h-full overflow-auto flex-grow md:m-10 p-10 flex flex-col gap-5 justify-center'>
-                    <div className='w-48 h-48 bg-blue-300'></div>
-                    <div className='flex flex-col'>
-                        <div className='text-sm'>Title</div>
-                        <div className='font-bold text-lg'>Introduction to Algorithm</div>
+                <div className='bg-neutral-800 text-neutral-100 border-2 border-teal-100 rounded-md max-h-full overflow-auto flex flex-col gap-5 flex-grow md:m-10 p-5  justify-center'>
+                    <div className='flex justify-between'>
+                        <div className='min-w-48 h-48 bg-blue-300'></div>
+                        <div className='p-10 pt-0 flex-grow overflow-y-hidden'>{description}</div>
                     </div>
-                    <div className='flex flex-col'>
-                        <div className='text-sm'>Author</div>
-                        <div className='font-bold tex-lg'>Dr. Somores Bosu</div>
-                    </div>
-                    <div className='flex flex-col'>
-                        <div className='text-sm'>Availablity</div>
-                        <div className='font-bold text-lg'>Available</div>
+                    <div className='flex gap-20'>
+                        <div className='flex flex-col gap-5'>
+                            <div className='flex flex-col'>
+                                <div className='text-sm text-neutral-400'>Title</div>
+                                <div className='font-bold text-lg'>{title}</div>
+                            </div>
+                            <div className='flex flex-col'>
+                                <div className='text-sm text-neutral-400'>Author</div>
+                                <div className='font-bold tex-lg'>{author}</div>
+                            </div>
+                            <div className='flex flex-col'>
+                                <div className='text-sm text-neutral-400'>Availablity</div>
+                                <div className='font-bold text-lg'>{availablity}</div>
+                            </div>
+                        </div>
+                        <div className='flex flex-col gap-5'>
+                            <div className='flex flex-col'>
+                                <div className='text-sm text-neutral-400'>Publish Year</div>
+                                <div className='font-bold text-lg'>{pyear}</div>
+                            </div>
+                            <div className='flex flex-col'>
+                                <div className='text-sm text-neutral-400'>Edition</div>
+                                <div className='font-bold tex-lg'>{edition}</div>
+                            </div>
+                        </div>
                     </div>
                     <div className='flex justify-center'>
                         <button onClick={handleOrder}
--- src/components/sidnav/SideNavBar.tsx
+++ src/components/sidnav/SideNavBar.tsx
@@ -8,7 +8,7 @@ import { CgProfile } from "react-icons/cg";
 
 export default function SideNavBar() {
     return (
-        <div className="flex w-48 bg-teal-800 text-teal-100 font-semibold ps-8 flex-col h-full justify-center gap-10 items-start">
+        <div className="flex w-48 p-5 bg-teal-800 text-teal-100 font-semibold ps-8 flex-col h-full justify-center gap-10 items-start">
             <div className="flex cursor-pointer hover:bg-teal-900 w-full py-2 ps-2  items-center gap-5">
                 <CgProfile size={25} />
                 <div>Profile</div>
