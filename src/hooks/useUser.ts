5a8823ad880f4bba5b79bcf143869fd906c9d248
Fahim Adnan <59190871+adnangif@users.noreply.github.com>
2024-03-04 00:41:28 +0600

    added frontend user authentication hooks

---
+++ src/hooks/useUser.ts
@@ -0,0 +1,25 @@
+import { useContext } from "react";
+import { AuthContext } from "../context/AuthContext";
+import { useLocalStorage } from "./useLocalStorage";
+
+export interface User {
+    id: number;
+    email: string;
+}
+
+export const useUser = () => {
+    const { user, setUser } = useContext(AuthContext);
+    const { setItem } = useLocalStorage();
+
+    const addUser = (user: User) => {
+        setUser(user);
+        setItem("user", JSON.stringify(user));
+    };
+
+    const removeUser = () => {
+        setUser(null);
+        setItem("user", "");
+    };
+
+    return { user, addUser, removeUser };
+};
