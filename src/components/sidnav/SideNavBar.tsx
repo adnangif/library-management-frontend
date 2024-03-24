9e9e4079373fee950cae48043c47a981b7d8cc53
Fahim Adnan <muhammadfahim010@gmail.com>
2024-03-25 05:48:14 +0600

    Update SideNavBar.tsx
    added navigation button destination logics

--- src/components/sidnav/SideNavBar.tsx
+++ src/components/sidnav/SideNavBar.tsx
@@ -6,42 +6,61 @@ import { CiLogout } from "react-icons/ci";
 import { CgProfile } from "react-icons/cg";
 import { clearGlobalUser } from "../../userManagement";
 import {useNavigate } from "react-router-dom";
+import { useRef } from "react";
 
 
 export default function SideNavBar() {
+
+    const profBtnRef = useRef(null)
+
     const nav = useNavigate()
 
     function handleLogout(){
         clearGlobalUser()
         nav('/login')
+    }
 
+    function handleProfile(){
+        nav('/profile')
+    }
+    function handleHome(){
+        nav('/home')
+    }
+    function handleSearch(){
+        nav('/search')
+    }
+    function handleCategory(){
+        nav('/category')
+    }
+    function handleOrders(){
+        nav('/orders')
     }
     return (
         <div className="flex w-48  bg-teal-800 text-teal-100 font-semibold ps-8 flex-col h-full justify-center gap-10 items-start">
-            <div className="flex cursor-pointer hover:bg-teal-900 w-full py-2 p-2  items-center gap-5">
+            <button onClick={handleProfile} className="flex cursor-pointer hover:bg-teal-900 w-full py-2 p-2  items-center gap-5">
                 <CgProfile size={25} />
                 <div>Profile</div>
-            </div>
+            </button>
 
-            <div className="flex cursor-pointer hover:bg-teal-900 w-full p-2  items-center gap-5">
+            <button onClick={handleHome} className="flex cursor-pointer hover:bg-teal-900 w-full p-2  items-center gap-5">
                 <CiHome size={25} />
                 <div>Home</div>
-            </div>
+            </button>
 
-            <div className="flex cursor-pointer hover:bg-teal-900 w-full p-2   items-center gap-5">
+            <button onClick={handleSearch} className="flex cursor-pointer hover:bg-teal-900 w-full p-2   items-center gap-5">
                 <CiSearch size={25} />
                 <div>Search</div>
-            </div>
+            </button>
 
-            <div className="flex cursor-pointer hover:bg-teal-900 w-full p-2  items-center gap-5">
+            <button onClick={handleCategory} className="flex cursor-pointer hover:bg-teal-900 w-full p-2  items-center gap-5">
                 <TbCategory2 size={25} />
                 <div>Categories</div>
-            </div>
+            </button>
 
-            <div className="flex cursor-pointer hover:bg-teal-900 w-full p-2  items-center gap-5">
+            <button onClick={handleOrders} className="flex cursor-pointer hover:bg-teal-900 w-full p-2  items-center gap-5">
                 <CgFileDocument size={25} />
                 <div>Orders</div>
-            </div>
+            </button>
 
             <button onClick={handleLogout} className="flex cursor-pointer hover:bg-teal-900 w-full p-2  items-center gap-5">
                 <CiLogout size={25} />
