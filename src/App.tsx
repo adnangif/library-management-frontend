5cee017a2d80fa93169d8b23e142851397c17643
Fahim Adnan <muhammadfahim010@gmail.com>
2024-04-15 20:01:35 +0600

    added useQuery

--- src/App.tsx
+++ src/App.tsx
@@ -1,88 +1,12 @@
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
-
-import BookDetailPage from "./components/book/BookDetailPage";
-import BorrowListPage from "./components/borrows/BorrowListPage";
-import CategoryMainPage from "./components/categories/CategoryMainPage";
-import CategorySubPage from "./components/categories/CategorySubPage";
-import ErrorPage from "./components/errors/ErrorPage";
-import LoginPage from "./components/login/LoginPage";
-import OrderDetailPage from "./components/order/OrderDetailPage";
-import OrderListPage from "./components/order/OrderListPage";
-import ProfilePage from "./components/profile/ProfilePage";
-import SearchPage from "./components/search/SearchPage";
-import SignupPage from "./components/signup/SignupPage";
-
+import { RouterProvider } from "react-router-dom";
+import Router from "./Routes";
 
 export default function App() {
-    const router = createBrowserRouter([
-        {
-            path: 'login',
-            element: <LoginPage />
-        },
-        {
-            path: 'home',
-            element: <BorrowListPage />
-        },
-        {
-            path: 'signup',
-            element: <SignupPage />
-        },
-        {
-            path: 'profile',
-            children: [
-                {
-                    path: '',
-                    element: <ProfilePage />
-                }
 
-            ]
-        },
-        {
-            path: 'book',
-            children: [
-                {
-                    path: '',
-                    element: <ErrorPage />
-                },
-                {
-                    path: ':bookid',
-                    element: <BookDetailPage />
-                },
-            ]
-        },
-        {
-            path: 'search',
-            element: <SearchPage />
-        },
-        {
-            path: 'category',
-            children: [
-                {
-                    path: '',
-                    element: <CategoryMainPage />,
-                },
-                {
-                    path: ':catid',
-                    element: <CategorySubPage />
-                }
-            ]
-        },
-        {
-            path: 'orders',
-            children: [
-                {
-                    path: '',
-                    element: <OrderListPage />,
-                },
-                {
-                    path: ':orderid',
-                    element: <OrderDetailPage />
-                }
-            ]
-        },
-    ])
 
     return (
-        <RouterProvider router={router} />
+        <>
+            <RouterProvider router={Router} />
+        </>
     )
 }
---
+++ src/Routes.tsx
@@ -0,0 +1,83 @@
+import { createBrowserRouter } from "react-router-dom";
+
+import BookDetailPage from "./components/book/BookDetailPage";
+import BorrowListPage from "./components/borrows/BorrowListPage";
+import CategoryMainPage from "./components/categories/CategoryMainPage";
+import CategorySubPage from "./components/categories/CategorySubPage";
+import ErrorPage from "./components/errors/ErrorPage";
+import LoginPage from "./components/login/LoginPage";
+import OrderDetailPage from "./components/order/OrderDetailPage";
+import OrderListPage from "./components/order/OrderListPage";
+import ProfilePage from "./components/profile/ProfilePage";
+import SearchPage from "./components/search/SearchPage";
+import SignupPage from "./components/signup/SignupPage";
+
+const Router = createBrowserRouter([
+    {
+        path: 'login',
+        element: <LoginPage />
+    },
+    {
+        path: 'home',
+        element: <BorrowListPage />
+    },
+    {
+        path: 'signup',
+        element: <SignupPage />
+    },
+    {
+        path: 'profile',
+        children: [
+            {
+                path: '',
+                element: <ProfilePage />
+            }
+
+        ]
+    },
+    {
+        path: 'book',
+        children: [
+            {
+                path: '',
+                element: <ErrorPage />
+            },
+            {
+                path: ':bookid',
+                element: <BookDetailPage />
+            },
+        ]
+    },
+    {
+        path: 'search',
+        element: <SearchPage />
+    },
+    {
+        path: 'category',
+        children: [
+            {
+                path: '',
+                element: <CategoryMainPage />,
+            },
+            {
+                path: ':catid',
+                element: <CategorySubPage />
+            }
+        ]
+    },
+    {
+        path: 'orders',
+        children: [
+            {
+                path: '',
+                element: <OrderListPage />,
+            },
+            {
+                path: ':orderid',
+                element: <OrderDetailPage />
+            }
+        ]
+    },
+])
+
+export default Router
